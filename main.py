1ba4d0a3fef3cde187d1b330bdfa2524da97821c
MishaLivshitz <sight1991@gmail.com>
2018-03-26 17:37:07 +0300

    institutes names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import urllib.request
-
+import re
 from bs4 import BeautifulSoup
 from Classes import Institute, Lecturer
 
@@ -10,26 +10,52 @@ def myencode(text):
             , 'e9': 'י', 'ea': 'ך', 'eb': 'כ', 'ec': 'ל', 'ed': 'ם', 'ee': 'מ', 'ef': 'ן', 'f0': 'נ',
          'f1': 'ס',
          'f2': 'ע', 'f3': 'ף', 'f4': 'פ', 'f5': 'ץ', 'f6': 'צ', 'f7': 'ק', 'f8': 'ר', 'f9': 'ש', 'fa': 'ת'});
-    soup = BeautifulSoup(text, 'html.parser').prettify();
+
+    # soup = BeautifulSoup(text, 'html.parser').prettify();
     for key, value in letters.items():
-        soup = soup.replace('\\x' + key, value);
-    print(soup);
-    return soup;
+        text = str(text).replace('\\x' + key, value);
+    # print(soup);
+    return text;
+
+
+def getinstitutes(dargooSource):
+    start = dargooSource.find("בחר מוסד לימודים")
+    end = dargooSource.find("</div>", start)
+    names = dargooSource[start:end]
+    size = names.count("<option value=")
+    for i in range(size):
+        id_index = names.find("<option value=") + 14
+        id_number = re.sub("[^0-9]", "", names[id_index:id_index + 3])
+        print(id_number)
+        if int(id_number) >= 10:
+            name_index = id_index + 3
+        else:
+            name_index = id_index + 2
+
+        names = names[name_index:]
+        name = names[0:names.find("</option")]
+        print(name)
+
+    # print(names)
+
+    return
 
 
 url = "http://www.dargoo.co.il/"
 request = urllib.request.Request(url)
 response = urllib.request.urlopen(request)
 
-check = Institute.Institute('Bruade', '23', {Lecturer.Lecturer(23, 'moshe'), Lecturer.Lecturer(25, 'David')})
-check1 = Institute.Institute('SCE', '27', {Lecturer.Lecturer(23, 'moshe')})
-check2 = Institute.Institute('TAU', '21', {Lecturer.Lecturer(23, 'moshe')})
-check4 = Institute.Institute('OBU', '8', {Lecturer.Lecturer(23, 'moshe')})
+getinstitutes(myencode(response.read()))
 
-instituteList = {check, check1, check2, check4}
+# check = Institute.Institute('Bruade', '23', {Lecturer.Lecturer(23, 'moshe'), Lecturer.Lecturer(25, 'David')})
+# check1 = Institute.Institute('SCE', '27', {Lecturer.Lecturer(23, 'moshe')})
+# check2 = Institute.Institute('TAU', '21', {Lecturer.Lecturer(23, 'moshe')})
+# check4 = Institute.Institute('OBU', '8', {Lecturer.Lecturer(23, 'moshe')})
 
-for i in instituteList:
-    print(i.__call__());
+# instituteList = {check, check1, check2, check4}
+
+# for i in instituteList:
+#   print(i.__call__());
 
 # with open("Output.txt", "w") as text_file:
 #   print(f"{myencode(response.read())}", file=text_file)
